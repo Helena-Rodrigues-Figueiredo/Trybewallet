f7132f3076c4f29646872c5f36ef2442759c3acd
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-06-22 19:50:01 -0300

    Requisito 7

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -6,17 +6,34 @@ class Wallet extends React.Component {
   render() {
     const { email } = this.props;
     return (
-      <header>
-        <p data-testid="email-field">
-          { email }
-        </p>
-        <p data-testid="total-field">
-          0
-        </p>
-        <p data-testid="header-currency-field">
-          BRL
-        </p>
-      </header>
+      <>
+        <header>
+          <p data-testid="email-field">
+            { email }
+          </p>
+          <p data-testid="total-field">
+            0
+          </p>
+          <p data-testid="header-currency-field">
+            BRL
+          </p>
+        </header>
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+        </table>
+      </>
     );
   }
 }
